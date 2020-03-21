65660ec06dc8a7cd473df9ea18c5002e4dcd8c89
LatikovAR <sslunatik@mail.ru>
2020-03-21 16:36:06 +0300

    eqcomp.c

--- eqcomp.c
+++ eqcomp.c
@@ -9,7 +9,7 @@ eqcomp() {
   double d1, d2, d3;
   d1 = 10.0;
   d2 = sqrt(d1);
-  d3 = d2 * d2;
+  d3 = d2 * d2 + d2 + d2;
   if (d1 != d3) {
     printf("d1 (%.15lf) != d3 (%.15lf)\n", d1, d3);
   }
